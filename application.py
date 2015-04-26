6a6f6f7a0d1d5442e0c457f3f191889930b5af29
Angela Castanieto <angela@Jeffs-Mac.local>
2015-04-25 18:45:35 -0700

    Began Flask app with application-form.html routing

--- application.py
+++ application.py
@@ -1,16 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index_page():
     # Return this as a strange
-    return "<html><body>This is the homepage.</body></html>"
+    # return "<html><body>This is the homepage.</body></html>"
 
     # Alternately, we could make this a Jinja template in `templates/`
     # and return that result of rendering this, like:
     #
-    # return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/application-form")
+def application_form():
+    return render_template("application-form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
